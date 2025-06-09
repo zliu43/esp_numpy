92d9aebd88574c50d76281da546e75c22033a693
zliu43 <zliu24601@gmail.com>
2025-06-08 20:05:54 -0700

    Initial Commit

---
+++ vector.h
@@ -0,0 +1,34 @@
+
+/*
+Efficient Vector Library targeting ESP32s3 chips,
+Using xtensa extended vector instructionsfor SIMD operations.
+
+Mike L.
+
+THIS IS A WORK IN PROGRESS
+*/
+
+#include <stdint.h>
+#include <stdlib.h>
+
+static inline size_t dtype_size(datatype dt) {
+    switch (dt) {
+        case DT_INT8: return 1;
+        case DT_INT16: return 2;
+        case DT_FLOAT16: return 2;
+        default: return 0;
+    }
+}
+
+enum datatype{
+    DT_INT8,
+    DT_INT16,
+    DT_FLOAT16
+};
+
+typedef struct{
+    size_t size,
+    void *data,
+    datatype type
+} vector;
+
